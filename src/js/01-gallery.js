d0c59abef0ee0f22f150cb386a03af71219d3c32
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-06 21:35:46 +0300

    finish 1st hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+// Рендер галереї
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Створення розмітки галереї
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
+    })
+    .join('');
+}
+
+const pictureGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
