51c1a99268739ed04a1886ad60a28e0ccda58780
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-09 09:35:46 +0300

    install throttle, vimeoLibrary, finish 2nd hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
 
+player.on('timeupdate', throttle(onVideoTimeSave, 1000));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+function onVideoTimeSave(evt) {
+  let currentTime = evt.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}
