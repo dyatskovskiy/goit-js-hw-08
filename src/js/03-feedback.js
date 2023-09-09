7d9cabaeb62d13a3dfb25703b87fc7536df94c4a
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-09 23:58:17 +0300

    finish 3rd hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,42 @@
-// Виконуй це завдання у файлах 03-feedback.html і 03-feedback.js. Розбий його на декілька підзавдань:
+import throttle from 'lodash.throttle';
 
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+const STORAGE_KEY = 'feedback-form-state';
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
 const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input');
+const messageTextArea = document.querySelector('.feedback-form textarea');
 
-form.addEventListener('input', onFormInput);
+//Слухачі подій
+emailInput.addEventListener('input', throttle(onFormInput, 500));
+messageTextArea.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
+populateTextArea();
+
+// Запис в локальне сховище даних з інпутів
 function onFormInput(evt) {
-  console.log(evt.target.type);
-  console.log(evt.target.value);
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({
+      email: emailInput.value,
+      message: messageTextArea.value,
+    })
+  );
+}
+
+//Заповнення полів форми збереженими даними за наявності таких даних
+function populateTextArea() {
+  if (savedData) {
+    emailInput.value = savedData.email;
+    messageTextArea.value = savedData.message;
+  }
+}
+
+// Перезавантаження форми при сабміті
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(savedData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
 }
